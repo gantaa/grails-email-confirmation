f81e36cddfa1602b043ef0e6b153b1e1d8f95b13
José Romeu García <jose.romeu@tecnocom.es>
2016-10-16 19:16:52 +0200

    Adapted for grails 3.1.11

--- grails-app/controllers/com/grailsrocks/emailconfirmation/EmailConfirmationController.groovy
+++ grails-app/controllers/com/grailsrocks/emailconfirmation/EmailConfirmationController.groovy
@@ -16,6 +16,7 @@
 package com.grailsrocks.emailconfirmation
 
 import grails.util.GrailsUtil
+import reactor.bus.EventBus;
 
 class EmailConfirmationController {
 
@@ -23,19 +24,15 @@ class EmailConfirmationController {
 	
 	// Look up the supplied code and tell view what outcome was
 	def index = {
-		if (log.traceEnabled) log.trace("Email confirmation controller invoked with token ${params.id}")
+		if (log.infoEnabled) 
+			log.info("Email confirmation controller invoked with token ${params.id}")
+			
 		
 		// We should not decode params.id this but this is a hack for Grails 1.0.2 bug
 		def result = emailConfirmationService.checkConfirmation(params.id)
 
         // if callback specified args, do a redirect or closure invoke instead of our default view
 		if ( result.actionToTake ) {
-        	pluginFlash.success = result.valid
-        	if (result.valid) {
-            	pluginFlash.email = result.email
-            	pluginFlash.token = result.token
-        	}
-
             // Was the result a redirect args map?
             if (result.actionToTake instanceof Map) {
 		        redirect( result.actionToTake)
@@ -44,7 +41,9 @@ class EmailConfirmationController {
 	            def code = result.actionToTake.clone()
 	            code.delegate = this
 	            code.resolveStrategy = Closure.DELEGATE_FIRST
-	            code()
+	            def resultado = code()
+	        } else {
+				return [success: result.valid, email: result.email]
 	        }
 		} else {
 		    return [success: result.valid, email: result.email]
